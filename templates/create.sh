49b47b5036cf820d03f3a6265502a970a560c5df
d_poleselli <dpoleselli@outsideopen.com>
2022-01-08 22:03:22 -0600

    generalize cfn and update create script for root domain creation

--- templates/create.sh
+++ templates/create.sh
@@ -1,19 +1,34 @@
+#!/bin/bash
+
 if [ $# -eq 0 ]; then
     echo "Please provide a subdomain name"
     exit 1
 fi
 
-aws cloudformation create-stack \
-  --stack-name "$1-poleselli-sub-domain" \
-  --template-body file://cfn-main.yaml \
-  --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
-  --region us-east-1 \
-  --parameters \
-    ParameterKey=SubDomainName,ParameterValue="$1" \
-    ParameterKey=AcmCertificateArn,ParameterValue='arn:aws:acm:us-east-1:056961530812:certificate/a3b60e9d-9d8e-4c01-946b-a1b39ba2f864'
+if [ $# -ge 1 ] && [ "$1" = "--root" ]; 
+then
+   aws cloudformation create-stack \
+    --stack-name "poleselli-root-domain" \
+    --template-body file://cfn-main.yaml \
+    --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
+    --region us-east-1 \
+    --parameters \
+      ParameterKey=DomainName,ParameterValue="poleselli.com" \
+      ParameterKey=HostedZone,ParameterValue="poleselli.com" \
+      ParameterKey=AcmCertificateArn,ParameterValue="arn:aws:acm:us-east-1:056961530812:certificate/a3b60e9d-9d8e-4c01-946b-a1b39ba2f864"
+else
+  aws cloudformation create-stack \
+    --stack-name "$1-poleselli-sub-domain" \
+    --template-body file://cfn-main.yaml \
+    --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
+    --region us-east-1 \
+    --parameters \
+      ParameterKey=DomainName,ParameterValue="$1.poleselli.com" \
+      ParameterKey=HostedZone,ParameterValue="poleselli.com" \
+      ParameterKey=AcmCertificateArn,ParameterValue="arn:aws:acm:us-east-1:056961530812:certificate/a3b60e9d-9d8e-4c01-946b-a1b39ba2f864"
+fi
 
-if [ $# -eq 2 ] && [ "$2" -eq "--wait" ]; then
+if [ $# -eq 2 ] && [ "$2" = "--wait" ]; then
    aws cloudformation wait stack-create-complete \
         --stack-name "$1-poleselli-sub-domain"
-   exit;
 fi
